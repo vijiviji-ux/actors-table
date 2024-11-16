b41819d62e1d5102eef163e73ed9b48e8065bebe
vijiviji-ux <vjaambal29@gmail.com>
2024-11-16 12:31:45 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function view(){
+    console.log('test1');
+    document.getElementById('gihili').src="./images/vijay.jpg";
+
+}
+
+function view1(){
+    console.log('test2');
+    document.getElementById('vaali').src="./images/ajith.jpg";
+}
+
+function view2(){
+    console.log('test3');
+    document.getElementById('theri').src="./images/samantha.jpg";
+
+}
+
+function view3(){
+    console.log('test4');
+    document.getElementById('selvan').src="./images/trisha.jpg";
+
+}
+
+function view4(){
+    console.log('test5');
+    document.getElementById('vada').src="./images/dhanush.jpg";
+}
